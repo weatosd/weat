380f53a6a3ddc97c87d5f05ec4128e8cf4ce9728
LucasTJerez <ltj2105@columbia.edu>
2020-12-09 09:32:01 -0800

    added an constructor that builds out the customer's basic info using the api

---
+++ frontend/Customer.py
@@ -0,0 +1,35 @@
+import requests
+import socket
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)
+
+
+class Customer:
+    key = None
+    id = None
+    name = None
+    address = None
+
+    def __init__(self, username, password):
+
+        loginObject = {'username': username, 'password': password}
+        response = requests.post('http://127.0.0.1:5000/login', data = loginObject).json()
+        if type(response) == str:
+            raise ValueError(response)
+        
+        self.key = response['key']
+        self.id = response['id']
+
+        getByIdObject = {'key': self.key, 'id': self.id}
+
+        response = requests.get('http://127.0.0.1:5000/getById', data = getByIdObject).json()
+
+        if type(response) == str:
+            raise ValueError(response)
+        
+        self.name = response['name']
+        self.address = response['address']
+
+        
+
+
