fc50f77d2a3dce8550cb082a851ba9e7595a6485
LucasTJerez <ltj2105@columbia.edu>
2020-12-08 19:59:42 -0800

    updating the api, but I need to switch to api branch

--- backend/api.py
+++ backend/api.py
@@ -1,54 +1,42 @@
 from flask import Flask, request, jsonify
 from .Database import Database
+from uuid import uuid4
 
 app = Flask(__name__)
 
 db = Database()
 
+# keys given to logged-in users, maps a key to a userId
+loginKeys = {}
 
+
+# first post request user should call
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
-
-
     for login in db.data['logins']:
         if username == login['username'] and password == login['password']:
-            userData = db.getById(login['id'])
-            return jsonify({'message': userData})
+            # generate a login key for the user that it can use to use functions
+            key = generateKey()
+            userId = login['id']
+            # adding key,id to map of keys
+            loginKeys[key] = userId
+            return jsonify({'key': key, 'userData': db.getById(userId)})
     return jsonify(['incorrect user or pass, nice try punk'])
 
-    
+
+# right now the Flask app urls mimic Database.py object methods for simplicity
+# users will only be able to use these methods if they have a valid key
 
 
-# @app.route('/login', methods=['POST'])
-# def login_user():
-#     username = request.form['username']
-#     password = request.form['password']
-    
-#     user = User.find_by_username(username)
-    
-    # if user and check_password_hash(user.password, password):
-    #     return jsonify({'message': 'Password is correct'})  # You'll want to return a token that verifies the user in the future
-    # return jsonify({'error': 'User or password are incorrect'}), 401
+@app.route('/getById', methods=['POST'])   
+def generateKey():
+    newKey = uuid4()
+    while newKey in loginKeys:
+        newKey = uuid4()
+    return newKey
 
 
-
-
-# @app.route("/restaurants/<id>")
-# def getRestaurant(id):
-#     id = int(id)
-#     if id == None:
-#         restaurants = db.getRestaurants()
-#         jsonData = json.dumps(restaurants)
-#         return jsonData
-#     else:
-#         restaurant = db.getRestaurantById(id)
-#         # print(restaurant)
-#         return json.dumps(restaurant)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
